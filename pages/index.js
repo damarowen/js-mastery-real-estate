4fea5bb47e8fefca33509ff4919db3d5cea3d321
damarowen <damarowen@gmailcom>
2022-10-21 17:33:40 +0700

    tinggal loading,location filter, deploy

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
+import { baseUrl, fetchApi } from '../utils/fetchApi'
 
+import Property  from '../components/Property'
 
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
-    <Image src={imageUrl} width={500} height={300} />
+    <Image src={imageUrl} width={500} height={300} alt=""/>
     <Box p='5'>
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
       <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
@@ -18,8 +20,11 @@ export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, link
 );
 
 
-const Home = () => (
-  <Box>
+const Home = ({propertiesForSale, propertiesForRent}) => {
+
+
+  return (
+    <Box>
     <Banner
       purpose='RENT A HOME'
       title1='Rental Homes for'
@@ -30,6 +35,9 @@ const Home = () => (
       linkName='/search?purpose=for-rent'
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
     />
+      <Flex flexWrap='wrap'>
+      {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
+    </Flex>
     <Banner
       purpose='BUY A HOME'
       title1=' Find, Buy & Own Your'
@@ -40,19 +48,28 @@ const Home = () => (
       linkName='/search?purpose=for-sale'
       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
     />
+     <Flex flexWrap='wrap'>
+      {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
+    </Flex>
   </Box>
-);
+  )
+ 
+  };
 
-// export async function getStaticProps() {
-//   const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
-//   const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
 
-//   return {
-//     props: {
-//       propertiesForSale: propertyForSale?.hits,
-//       propertiesForRent: propertyForRent?.hits,
-//     },
-//   };
-// }
+
+  //* fetch API
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+  console.log(propertyForSale, "<<<<")
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
+}
 
 export default Home;
